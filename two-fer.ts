147de2e5035b0548cf5e5c44bd473861e54724fb
Rosie Highsmith <ahighsmith@flexion.us>
2023-10-12 07:41:02 -0700

    Squashed commit of the following:
    commit 0a5d490a5c33fdbb7931c516b8787933c0ce6822
    Author: Timothy Shumway <tshumway@flexion.us>
    Date:   Thu Oct 12 08:39:12 2023 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:two-fer.test.ts
    
    commit 323995c61f6f085f9d5318918e94bf01c138971a
    Author: Rosie Highsmith <ahighsmith@flexion.us>
    Date:   Thu Oct 12 07:35:11 2023 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:two-fer.ts
    
    commit 5401517f23f25bb4aaf78c4b27f5fd4d7766fc9c
    Author: Rosie Highsmith <ahighsmith@flexion.us>
    Date:   Thu Oct 12 07:30:12 2023 -0700
    
        mob start [ci-skip] [ci skip] [skip ci]
    Co-authored-by: Timothy Shumway <tshumway@flexion.us>

--- two-fer.ts
+++ two-fer.ts
@@ -1,3 +1,3 @@
-export const twoFer = () => {
-  throw new Error('Remove this statement and implement this function');
+export const twoFer = (name = 'you') => {
+  return `One for ${name}, one for me.`;
 };
