53d53827a7522ea4d8d0b9991241646916704779
Rosie Highsmith <ahighsmith@flexion.us>
2023-10-12 09:14:41 -0700

    Squashed commit of the following:
    commit a373f0b125a58dcdb186ad4544431faa98986c59
    Author: Rosie Highsmith <ahighsmith@flexion.us>
    Date:   Thu Oct 12 09:14:18 2023 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:two-fer.test.ts
    
    commit 611273ad80d58b568cdca87983695803241f5672
    Author: Timothy Shumway <tshumway@flexion.us>
    Date:   Thu Oct 12 10:13:23 2023 -0600
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:two-fer.ts
    
    commit 10d6002a7e0ea75fc364124c4c995adf70972790
    Author: Rosie Highsmith <ahighsmith@flexion.us>
    Date:   Thu Oct 12 09:11:22 2023 -0700
    
        mob next [ci-skip] [ci skip] [skip ci]
    
        lastFile:two-fer.test.ts
    
    commit 2b96d723bbfb64ab616c462b2e44cd84707207b3
    Author: Rosie Highsmith <ahighsmith@flexion.us>
    Date:   Thu Oct 12 09:10:05 2023 -0700
    
        mob start [ci-skip] [ci skip] [skip ci]
    Co-authored-by: Timothy Shumway <tshumway@flexion.us>

--- two-fer.test.ts
+++ two-fer.test.ts
@@ -6,11 +6,11 @@ describe('twoFer()', () => {
     expect(twoFer()).toEqual('One for you, one for me.');
   });
 
-  it.skip('a name given', () => {
+  it('a name given', () => {
     expect(twoFer('Alice')).toEqual('One for Alice, one for me.');
   });
 
-  it.skip('another name given', () => {
+  it('another name given', () => {
     expect(twoFer('Bob')).toEqual('One for Bob, one for me.');
   });
 });
